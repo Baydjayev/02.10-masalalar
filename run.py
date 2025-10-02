ad38c3681620fce33834c7e7d85b87380628e46b
Baydjayev Mulkomon <itmulkomon@gmail.com>
2025-10-02 16:35:18 +0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,162 @@
+#1
+a = 10
+b = 10
+summ = a + b
+
+print(summ)
+
+
+#2
+l = 15
+w = 10
+area = l * w
+
+print(area)
+
+
+#3
+x = 10
+y = 20
+z = 15
+avg = (x + y + z)
+
+print(avg)
+
+
+#4
+a = 20
+b = 20
+p = 2 * (a + b)
+
+print(p)
+
+
+#5
+h = 1230
+m = 123
+total_min = h * 60 + m 
+
+print(total_min)
+
+
+#6
+usd = 10000
+summ = usd * 12500
+
+print(summ)
+
+
+#7
+a = 160
+b = 150
+diff = abs(a - b)
+
+print(diff)
+
+
+#8
+x = 20
+n = 200
+power = x ** n
+
+print(power)
+
+
+#9
+a = 500
+b = 700
+quotient = a // b
+
+print(quotient)
+
+
+#10
+a = 10
+b = 20
+a, b = b, a
+
+print(a, b)
+
+
+#11
+total_rent = 150000000
+n = 24
+per_month =total_rent / n
+
+print(per_month)
+
+
+#12
+d = 500
+v = 80
+time = d / v
+
+print("vaqt:", time)
+
+
+#13
+seatsseats = 50
+passengers = 30
+per_person = passengers // seatsseats
+left = passengers % seatsseats
+
+print(per_person, left)
+
+
+#14
+consumption = 12
+d = 60
+fuel = consumption * d / 100
+
+print(fuel)
+
+
+#15
+salary = 2000000
+net = salary * (1 - 0.12)
+
+print(net)
+
+
+#16
+amount = 150000
+days = 7
+penalty = amount * 0.001 * days
+total = amount + penalty
+
+print(penalty, total)
+
+
+#17
+price = 150000
+discount = 15
+paid = price * (1-discount / 100)
+
+print(paid)
+
+
+#18
+p = 21
+r = 10
+t = 12
+tinterest =p * r / 100 * t
+total = p + interesttotal = p + tinterest
+
+print(total)
+
+
+#19
+base_qty = 1000
+people = 5
+per_person = base_qty / 4
+needed = per_person * people
+
+print(f"kishi: {people}  gramm: {needed}")
+
+
+#20
+bill = 200000
+tip = bill * 0.10
+total = bill + tip
+
+print(total)
